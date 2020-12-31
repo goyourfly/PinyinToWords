02c61b1077d5bf29b59b6d791c1a72566e189cd9
gaoyufei <goyourfly@gmail.com>
2020-12-31 15:28:14 +0800

    fix resource not fould bug

--- src/main/kotlin/com/yff/ptw/provider/DefaultPathProvider.kt
+++ src/main/kotlin/com/yff/ptw/provider/DefaultPathProvider.kt
@@ -1,10 +1,26 @@
 package com.yff.ptw.provider
 
+import java.io.File
+import java.io.FileOutputStream
 import java.nio.file.Paths
 
 class DefaultPathProvider : PathProvider {
     override fun getDictPath(): String {
-        return Paths.get("").toAbsolutePath().toString() + "/src/main/resources/webdict_with_freq_with_pinyin.txt"
+        // copy to cache file
+        val outFile = File(getCacheDir() + "/webdict_with_freq_with_pinyin.txt")
+        if(outFile.exists() && outFile.length() > 10){
+            return outFile.path
+        }
+        if(!outFile.parentFile.exists()){
+            outFile.parentFile.mkdirs()
+        }
+        outFile.createNewFile()
+        Thread.currentThread().contextClassLoader.getResourceAsStream("webdict_with_freq_with_pinyin.txt").use { inputStream ->
+            FileOutputStream(outFile).use { fileOutputStream ->
+                inputStream?.copyTo(fileOutputStream)
+            }
+        }
+        return outFile.path
     }
 
     override fun getCacheDir(): String {
